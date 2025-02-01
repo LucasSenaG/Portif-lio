2b2f56e29fc4e8e1b022040bbce33fd6c92e494c
Lucas Sena <lukasgirundi@gmail.com>
2025-01-31 21:19:20 -0300

    Atualização de layout pt1

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,80 @@
-// EFEITO PARTICULAS SE MOVIMENTANDO
-document.addEventListener("DOMContentLoaded", function () {
-    const canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
+// // EFEITO PARTICULAS SE MOVIMENTANDO
+// document.addEventListener("DOMContentLoaded", function () {
+//     const canvas = document.createElement("canvas");
+//     document.body.appendChild(canvas);
 
-    const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+//     const ctx = canvas.getContext("2d");
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
 
-    const particles = [];
-    const particleCount = 50;
+//     const particles = [];
+//     const particleCount = 50;
 
-    for (let i = 0; i < particleCount; i++) {
-        particles.push({
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height,
-            size: Math.random() * 5 + 1,
-            speedX: Math.random() * 3 - 1.5,
-            speedY: Math.random() * 3 - 1.5,
-        });
-    }
+//     for (let i = 0; i < particleCount; i++) {
+//         particles.push({
+//             x: Math.random() * canvas.width,
+//             y: Math.random() * canvas.height,
+//             size: Math.random() * 5 + 1,
+//             speedX: Math.random() * 3 - 1.5,
+//             speedY: Math.random() * 3 - 1.5,
+//         });
+//     }
 
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     function draw() {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.fillStyle = "#64a6fc";
-        ctx.beginPath();
-        for (let i = 0; i < particleCount; i++) {
-            const particle = particles[i];
-            ctx.moveTo(particle.x, particle.y);
-            ctx.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2, true);
-        }
-        ctx.fill();
-        update();
-    }
+//         ctx.fillStyle = "#64a6fc";
+//         ctx.beginPath();
+//         for (let i = 0; i < particleCount; i++) {
+//             const particle = particles[i];
+//             ctx.moveTo(particle.x, particle.y);
+//             ctx.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2, true);
+//         }
+//         ctx.fill();
+//         update();
+//     }
 
-    function update() {
-        for (let i = 0; i < particleCount; i++) {
-            const particle = particles[i];
-            particle.x += particle.speedX;
-            particle.y += particle.speedY;
+//     function update() {
+//         for (let i = 0; i < particleCount; i++) {
+//             const particle = particles[i];
+//             particle.x += particle.speedX;
+//             particle.y += particle.speedY;
 
-            if (particle.size > 0.2) particle.size -= 0.1;
-            if (particle.size <= 0.2) {
-                particle.size = Math.random() * 5 + 1;
-                particle.x = Math.random() * canvas.width;
-                particle.y = Math.random() * canvas.height;
-            }
-        }
-    }
+//             if (particle.size > 0.2) particle.size -= 0.1;
+//             if (particle.size <= 0.2) {
+//                 particle.size = Math.random() * 5 + 1;
+//                 particle.x = Math.random() * canvas.width;
+//                 particle.y = Math.random() * canvas.height;
+//             }
+//         }
+//     }
 
-    function animate() {
-        draw();
-        requestAnimationFrame(animate);
-    }
+//     function animate() {
+//         draw();
+//         requestAnimationFrame(animate);
+//     }
 
-    animate();
+//     animate();
 
-    window.addEventListener("resize", function () {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    });
-});
+//     window.addEventListener("resize", function () {
+//         canvas.width = window.innerWidth;
+//         canvas.height = window.innerHeight;
+//     });
+// });
 
 
-// EFEITO NO NOME 'LUCAS SENA' NO INÍCIO DA PÁGINA
-document.addEventListener("DOMContentLoaded", function () {
-    const title = document.querySelector('h1');
+// // EFEITO NO NOME 'LUCAS SENA' NO INÍCIO DA PÁGINA
+// document.addEventListener("DOMContentLoaded", function () {
+//     const title = document.querySelector('h1');
 
-    title.addEventListener('mouseover', function () {
-        title.style.color = '#092f49';
-    });
+//     title.addEventListener('mouseover', function () {
+//         title.style.color = '#092f49';
+//     });
 
-    title.addEventListener('mouseout', function () {
-        title.style.color = '#333';
-    });
-});
+//     title.addEventListener('mouseout', function () {
+//         title.style.color = '#333';
+//     });
+// });
 
 
 // EFEITO DE MOSTRAR E ESCONDER DIV COM MAIS INFORMAÇÕES 'SOBRE MIM'
